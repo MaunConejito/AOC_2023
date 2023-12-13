843cc0054e6c81abe1b9ac246b52dc6b3e53cd4a
Manuel Rammler <mrammler@scopeland.de>
2023-12-13 21:02:32 +0100

    add output directory if not existing

--- common/file_management/read_and_write.py
+++ common/file_management/read_and_write.py
@@ -1,4 +1,4 @@
-from os import path, listdir
+from os import path, listdir, makedirs
 
 INPUT_REL_PATH = '..\\data\\input\\'
 OUTPUT_REL_PATH = '..\\data\\output\\'
@@ -18,5 +18,8 @@ def get_file_content(absolute_path: str) -> str:
         return file.read()
 
 def write_file(absolute_path: str, content: str) -> str:
+    directory = path.dirname(absolute_path)
+    if not path.exists(directory):
+        makedirs(directory)
     with open(absolute_path, 'w+') as file:
         return file.write(content)
