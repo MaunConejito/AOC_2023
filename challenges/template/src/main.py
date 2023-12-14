94ebedc180d2b746c665cf94426652c4ab5ca665
Manuel Rammler <mrammler@scopeland.de>
2023-12-14 14:16:15 +0100

    improve template

--- challenges/template/src/main.py
+++ challenges/template/src/main.py
@@ -9,5 +9,5 @@ for DATA_FILE_NAME in DATA_FILE_NAMES:
 
     input = read_data(SCRIPT_PATH, DATA_FILE_NAME)
 
-    write_data(SCRIPT_PATH, DATA_FILE_NAME + '_1', process_1(input))
-    write_data(SCRIPT_PATH, DATA_FILE_NAME + '_2', process_2(input))
+    write_data(SCRIPT_PATH, '.\\part_1\\'+DATA_FILE_NAME, process_1(input))
+    write_data(SCRIPT_PATH, '.\\part_2\\'+DATA_FILE_NAME, process_2(input))
